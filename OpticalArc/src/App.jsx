d42380623c7d9cf699f898d91f878437772a5d90
mayurkukade <mayurkukade@opticalarc.com>
2023-05-08 17:20:31 +0530

    first

---
+++ OpticalArc/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar/Navbar"
+
+
+const App = () => {
+  return (
+  <>
+  <Navbar/>
+  </>
+  )
+}
+
+export default App
---
+++ OpticalArc/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+  return (
+    <section>
+      <h3>Optical Arc</h3>
+     
+    </section>
+  )
+}
+
+export default Navbar
